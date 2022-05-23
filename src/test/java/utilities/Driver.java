f434bf7b4a62a0a1f469863266a8589f1cad90ed
mmamyt2 <mmamyt2@uic.edu>
2022-05-23 16:41:22 -0600

    1

---
+++ src/test/java/utilities/Driver.java
@@ -0,0 +1,46 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Driver {
+    private Driver(){
+
+    }
+
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        if(driver == null){
+            switch(Config.getProperty("browser")){
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                case "edge":
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+            }
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        }
+        return driver;
+    }
+
+    public static void closeDriver() {
+        if (driver != null) {
+            //driver.close(); //closes one tab
+            driver.quit(); //closes all tabs
+        }
+    }
+}
---
+++ src/test/resources/features/google.feature
@@ -0,0 +1,7 @@
+@regression
+
+Feature: Google feature test verification
+  Here we can add some details about the test
+  #to comment
+  #Given, When, Then, And
+
