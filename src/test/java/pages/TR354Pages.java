17f9c11a831103b53ed34869429ea108472e411d
anisakalonova <akalonova@inths.org>
2022-06-04 10:34:26 -0500

    my updates

---
+++ src/test/java/pages/TR354Pages.java
@@ -0,0 +1,24 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class TR354Pages {
+    public TR354Pages(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath="//*[@id='search_query_top']")
+    public WebElement search;
+
+    @FindBy(xpath="//*[@id='searchbox']/button")
+    public WebElement searchButton;
+
+    @FindBy(xpath="//*[@id='center_column']")
+    public WebElement tshirt;
+
+
+
+}
---
+++ src/test/resources/features/TR361.feature
@@ -0,0 +1,8 @@
+@TR361
+Feature: Contact us functionality
+
+  Scenario: As a user, I want to be able to contact customer service
+    Given I am at the home page
+    Then I click on contact us
+    And I should be directed to customer service page
+    And I should be able to send a message to customer service team
